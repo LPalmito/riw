bb4cf6be8c6a98e1bb0082ed197792470cceaf8a
L Palmito <yannickmorel1@gmail.com>
2016-11-28 17:14:02 +0100

    Added the render_documents method and answered question 4

--- main_cacm.py
+++ main_cacm.py
@@ -1,18 +1,31 @@
 import nltk
 import math
+import matplotlib.pyplot as plt
 
 
 def render_documents(tokens):
+    # Initialisations
     markers = ['.I', '.T', '.W', '.B', '.A', '.N', '.X', '.K']
-    current_marker = ''
-
-    doc = {}
-    for i, token in enumerate(tokens):
-        if token in markers:
+    current_marker = '.I'
+    current_doc = {}
+    result = []
+    # Prepare the result
+    for token in tokens:
+        # For each new .I, prepare a new doc and fill it
+        if token == '.I':
+            if current_doc != {}:
+                result.append(current_doc)
+            current_doc = {}
+            for marker in markers:
+                current_doc[marker] = []
+        # For each other marker, update the current marker
+        elif token in markers:
             current_marker = token
+        # Else, fill the corresponding attribute
         else:
-            doc[current_marker].push(token)
-    test = doc
+            current_doc[current_marker].append(token)
+    result.append(current_doc)
+    return result
 
 if __name__ == '__main__':
 
@@ -20,31 +33,43 @@ if __name__ == '__main__':
     cacm = open('./Resources/CACM/cacm.all')
     text = cacm.read()
 
-    # Tokenize the text
+    # Tokenize the text and render in docs
     tokens = nltk.word_tokenize(text)
-    print("100 tokens:", tokens[:99])
-    # # Question 1 :
-    # T = len(tokens)
-    # print("Le nombre T de tokens est : ", T)
-    #
-    # # Find the vocabulary
-    # unique_tokens = list(set(tokens))
-    # # Question 2 :
-    # M = len(unique_tokens)
-    # print("La taille M du vocabulaire est :", M)
-    #
-    # # Take half of the collection
-    # half_tokens = tokens[:int(len(tokens)/2)]
-    # unique_half_tokens = list(set(half_tokens))
-    # # Question 3 :
-    # T2 = len(half_tokens)
-    # print("Le nombre T2 de tokens pour la moitié de la collection est : ", T2)
-    # M2 = len(unique_half_tokens)
-    # print("La taille M2 du vocabulaire pour la moitié de la collection est :", M2)
-    # b = math.log(M/M2, 2)
-    # print("b = ", b)
-    # k = M/T**b
-    # print("k = ", k)
+    docs = render_documents(tokens)
+
+    # Keep only the tokens in useful attributes
+    useful_tokens = []
+    for doc in docs:
+        useful_tokens.extend(doc['.T'])
+        useful_tokens.extend(doc['.W'])
+        useful_tokens.extend(doc['.K'])
+
+    # Question 1 :
+    T = len(useful_tokens)
+    print("Le nombre T de tokens dans .T, .W, .K est : ", T)
+
+    # Find the vocabulary
+    unique_useful_tokens = list(set(useful_tokens))
+    # Question 2 :
+    M = len(unique_useful_tokens)
+    print("La taille M du vocabulaire est :", M)
+
+    # Take half of the collection
+    half_useful_tokens = useful_tokens[:int(len(useful_tokens)/2)]
+    unique_half_useful_tokens = list(set(half_useful_tokens))
+    # Question 3 :
+    T2 = len(half_useful_tokens)
+    print("Le nombre T2 de tokens dans .T, .W, .K pour la moitié de la collection est : ", T2)
+    M2 = len(unique_half_useful_tokens)
+    print("La taille M2 du vocabulaire pour la moitié de la collection est :", M2)
+    b = math.log(M/M2, 2)
+    print("b = ", b)
+    k = M/T**b
+    print("k = ", k)
+
+    # Question 4 :
+    M_1M = math.floor(k*1000000**b)
+    print("La taille M_1M du vocabulaire pour 1 million de tokens est : ", M_1M)
 
     # Close cacm.all
     cacm.close()
