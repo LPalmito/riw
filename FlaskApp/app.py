b87124e2c61f8314706b71c6dfec56e370213970
Gus <augustin.courtier@gmail.com>
2017-01-12 17:19:42 +0100

    engine working on localhost via the app – not returning all results though. added F_Booleen_Front.py : adapted method for Booleen Search

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from riw.cacm.C_modele_booleen import *
+from riw.cacm.F_Booleen_Front import *
+from ast import literal_eval
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,8 +9,22 @@ def main():
 
 @app.route('/search')
 def search():
+    # Define parameter of the search
     params = request.args.get("search")
-    return params
+    params = params.upper()
+    with open("../FlaskApp/documents/docID_doc.json", "r") as doc:
+        docID_doc_string = doc.read()
+        docID_doc = literal_eval(docID_doc_string)
+    with open("../FlaskApp/documents/docs_backup.json", "r") as doc:
+        docs_backup_string = doc.read()
+        docs_backup = literal_eval(docs_backup_string)
+    with open("../FlaskApp/documents/term_termID.json", "r") as doc:
+        term_termID_string = doc.read()
+        term_termID = literal_eval(term_termID_string)
+    with open("../FlaskApp/documents/termID_docID.json", "r") as doc:
+        termID_docID_string = doc.read()
+        termID_docID = literal_eval(termID_docID_string)
+    return modele_booleen_front(term_termID, docID_doc, termID_docID, docs_backup, params)
 
 if __name__ == "__main__":
     app.run()
