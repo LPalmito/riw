9f506c2dd83b8d4bdb71e4060b425a172ed3be2b
Gus <augustin.courtier@gmail.com>
2017-02-17 18:09:15 +0100

    Refactored query_updates

--- cacm/D_modele_vectoriel.py
+++ cacm/D_modele_vectoriel.py
@@ -1,6 +1,7 @@
 import math
 from cacm.C_modele_booleen import *
 from cacm.print_tools import vectorial_print
+from cacm.query_updates import *
 
 
 def modele_vectoriel(term_termID, docID_doc, termID_docID, docID_termID, docs_backup):
@@ -119,41 +120,6 @@ def get_w_query(tID_dID, N_docs, N_terms, method, dID_mtf):
             w_query.append(n_freq(t_ID, -1, tID_dID, dID_mtf))
             s_query += w_query[t_ID] ** 2
     return w_query, s_query
-
-
-def update_termID_docID(query, termID_docID, term_termID):
-    """Update the termID_docID with the query"""
-    q_tokens = list(set([x.upper() for x in query.split()]))
-    tID_dID = termID_docID
-    for q_token in q_tokens:
-        if q_token in term_termID.keys():
-            tID_dID[term_termID[q_token]].append(-1)
-    return tID_dID
-
-
-def update_docID_termID(query, docID_termID, term_termID):
-    """Update the docID_termID with the query"""
-    q_tokens = list(set([x.upper() for x in query.split()]))
-    dID_tID = docID_termID
-    dID_tID[-1] = []
-    for q_token in q_tokens:
-        if q_token in term_termID.keys():
-            dID_tID[-1].append(term_termID[q_token])
-    return dID_tID
-
-
-def update_docID_mtf(docID_mtf, dID_tID, tID_dID):
-    """Update the docID_termID with the query"""
-    dID_mtf = docID_mtf
-    dID_mtf[-1] = 0
-    for t_ID in dID_tID[-1]:
-        tfj = 0
-        for dID in tID_dID[t_ID]:
-            if dID == dID:
-                tfj += 1
-        if tfj > docID_mtf[dID]:
-            docID_mtf[dID] = tfj
-    return dID_mtf
 
 
 def get_docID_mtf(docID_doc, docID_termID, termID_docID, m):
